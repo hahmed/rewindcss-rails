b31313fb7ff98ddc51eb87fc645b37324d051466
Haroon Ahmed <haroon.ahmed25@gmail.com>
2024-05-29 23:47:59 +0100

    Add initializer for deadfire and add normalizer to reset the css

--- lib/rewindcss/engine.rb
+++ lib/rewindcss/engine.rb
@@ -1,5 +1,21 @@
 module Rewindcss
   class Engine < ::Rails::Engine
     isolate_namespace Rewindcss
+
+    after_initialize do |app|
+      Deadfire.configure do |config|
+        config.root_path = Rails.root.join("app", "assets", "stylesheets")
+      end
+    end
+
+    after_initialize do |app|
+      class DeadfireCompiler < Propshaft::Compiler
+        def compile(logical_path, input)
+          Deadfire.parse(input)
+        end
+      end
+      
+      app.config.assets.compilers << ["text/css", DeadfireCompiler]
+    end
   end
 end
